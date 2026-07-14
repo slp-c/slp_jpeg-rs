d3d73cd9871cfb612915a7b1edd57f751a60d32d
slp-c <222554062+slp-c@users.noreply.github.com>
2026-07-15 05:05:08 +0700

    added some new algorithm. No test made

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -115,34 +115,33 @@ where
 
 pub fn convert_ycbcr2rgb(image: &mut [u8]) {
     type T = i32;
-
     const SHIFT: usize = size_of::<T>() * 8 / 2;
+    const A: [[T; 3]; 3] = {
+        #[rustfmt::skip]
+        const A_FLOAT: [[f64; 3]; 3] = [
+            [1.0, 0.0, 1.402],
+            [1.0, -0.34414, -0.71414],
+            [1.0, 1.772, 0.0]
+        ];
+
+        let mut x: [[T; 3]; 3] = [[0; 3]; 3];
+        let mut i = 0;
+        while i < 3 {
+            let mut j = 0;
+            while j < 3 {
+                x[i][j] = (A_FLOAT[i][j] * (1u64 << SHIFT) as f64) as T;
+                j += 1;
+            }
+            i += 1;
+        }
+
+        x
+    };
+
     for pixel in image.as_chunks_mut::<3>().0 {
         let y = pixel[0] as T;
         let cb = pixel[1] as T - 128;
         let cr = pixel[2] as T - 128;
-
-        const A: [[T; 3]; 3] = {
-            #[rustfmt::skip]
-            const A_FLOAT: [[f64; 3]; 3] = [
-                [1.0, 0.0, 1.402],
-                [1.0, -0.34414, -0.71414],
-                [1.0, 1.772, 0.0]
-            ];
-
-            let mut x: [[T; 3]; 3] = [[0; 3]; 3];
-            let mut i = 0;
-            while i < 3 {
-                let mut j = 0;
-                while j < 3 {
-                    x[i][j] = (A_FLOAT[i][j] * (1u64 << SHIFT) as f64) as T;
-                    j += 1;
-                }
-                i += 1;
-            }
-
-            x
-        };
 
         #[rustfmt::skip]
         {
@@ -152,36 +151,81 @@ pub fn convert_ycbcr2rgb(image: &mut [u8]) {
         };
     }
 }
+pub fn extract_ycbcr2rgb(image: &[u8], r: &mut [u8], g: &mut [u8], b: &mut [u8]) {
+    /*
+    assume r, g, b have the same len and panic if not
+    */
+    type T = i32;
+    const SHIFT: usize = size_of::<T>() * 8 / 2;
+    const A: [[T; 3]; 3] = {
+        #[rustfmt::skip]
+        const A_FLOAT: [[f64; 3]; 3] = [
+            [1.0, 0.0, 1.402],
+            [1.0, -0.34414, -0.71414],
+            [1.0, 1.772, 0.0]
+        ];
+
+        let mut x: [[T; 3]; 3] = [[0; 3]; 3];
+        let mut i = 0;
+        while i < 3 {
+            let mut j = 0;
+            while j < 3 {
+                x[i][j] = (A_FLOAT[i][j] * (1u64 << SHIFT) as f64) as T;
+                j += 1;
+            }
+            i += 1;
+        }
+
+        x
+    };
+
+    let mut r = r.iter_mut();
+    let mut g = g.iter_mut();
+    let mut b = b.iter_mut();
+
+    for pixel in image.as_chunks::<3>().0 {
+        let y = pixel[0] as T;
+        let cb = pixel[1] as T - 128;
+        let cr = pixel[2] as T - 128;
+
+        let r = r.next().unwrap();
+        let g = g.next().unwrap();
+        let b = b.next().unwrap();
+
+        *r = ((A[0][0] * y + A[0][1] * cb + A[0][2] * cr) >> SHIFT).clamp(0, 255) as u8;
+        *g = ((A[1][0] * y + A[1][1] * cb + A[1][2] * cr) >> SHIFT).clamp(0, 255) as u8;
+        *b = ((A[2][0] * y + A[2][1] * cb + A[2][2] * cr) >> SHIFT).clamp(0, 255) as u8;
+    }
+}
 pub fn convert_rgb2ycbcr(image: &mut [u8]) {
     type T = i32;
-
     const SHIFT: usize = size_of::<T>() * 8 / 2;
+    const A: [[T; 3]; 3] = {
+        #[rustfmt::skip]
+        const A_FLOAT: [[f64; 3]; 3] = [
+            [0.299, 0.587, 0.114],
+            [-0.168736, -0.331264, 0.5],
+            [0.5, -0.418688, -0.081312]
+        ];
+
+        let mut x: [[T; 3]; 3] = [[0; 3]; 3];
+        let mut i = 0;
+        while i < 3 {
+            let mut j = 0;
+            while j < 3 {
+                x[i][j] = (A_FLOAT[i][j] * (1u64 << SHIFT) as f64) as T;
+                j += 1;
+            }
+            i += 1;
+        }
+
+        x
+    };
+
     for pixel in image.as_chunks_mut::<3>().0 {
         let r = pixel[0] as T;
         let g = pixel[1] as T;
         let b = pixel[2] as T;
-
-        const A: [[T; 3]; 3] = {
-            #[rustfmt::skip]
-            const A_FLOAT: [[f64; 3]; 3] = [
-                [0.299, 0.587, 0.114],
-                [-0.168736, -0.331264, 0.5],
-                [0.5, -0.418688, -0.081312]
-            ];
-
-            let mut x: [[T; 3]; 3] = [[0; 3]; 3];
-            let mut i = 0;
-            while i < 3 {
-                let mut j = 0;
-                while j < 3 {
-                    x[i][j] = (A_FLOAT[i][j] * (1u64 << SHIFT) as f64) as T;
-                    j += 1;
-                }
-                i += 1;
-            }
-
-            x
-        };
 
         #[rustfmt::skip]
         {
@@ -190,4 +234,83 @@ pub fn convert_rgb2ycbcr(image: &mut [u8]) {
             pixel[2] = (((A[2][0] * r + A[2][1] * g + A[2][2] * b) >> SHIFT) + 128).clamp(0, 255) as u8;
         };
     }
+}
+pub fn extract_rgb2ycbcr(image: &[u8], y: &mut [u8], cb: &mut [u8], cr: &mut [u8]) {
+    /*
+    assume y, cb, cr have the same len and panic if not
+    */
+    type T = i32;
+    const SHIFT: usize = size_of::<T>() * 8 / 2;
+    const A: [[T; 3]; 3] = {
+        #[rustfmt::skip]
+        const A_FLOAT: [[f64; 3]; 3] = [
+            [0.299, 0.587, 0.114],
+            [-0.168736, -0.331264, 0.5],
+            [0.5, -0.418688, -0.081312]
+        ];
+
+        let mut x: [[T; 3]; 3] = [[0; 3]; 3];
+        let mut i = 0;
+        while i < 3 {
+            let mut j = 0;
+            while j < 3 {
+                x[i][j] = (A_FLOAT[i][j] * (1u64 << SHIFT) as f64) as T;
+                j += 1;
+            }
+            i += 1;
+        }
+
+        x
+    };
+
+    let mut y = y.iter_mut();
+    let mut cb = cb.iter_mut();
+    let mut cr = cr.iter_mut();
+
+    for pixel in image.as_chunks::<3>().0 {
+        let r = pixel[0] as T;
+        let g = pixel[1] as T;
+        let b = pixel[2] as T;
+
+        let y = y.next().unwrap();
+        let cb = cb.next().unwrap();
+        let cr = cr.next().unwrap();
+
+        *y = ((A[0][0] * r + A[0][1] * g + A[0][2] * b) >> SHIFT).clamp(0, 255) as u8;
+        *cb = (((A[1][0] * r + A[1][1] * g + A[1][2] * b) >> SHIFT) + 128).clamp(0, 255) as u8;
+        *cr = (((A[2][0] * r + A[2][1] * g + A[2][2] * b) >> SHIFT) + 128).clamp(0, 255) as u8;
+    }
+}
+
+pub fn subsampling(src: &[u8], i_factor: usize, j_factor: usize, width: usize) -> Vec<u8> {
+    if i_factor == 0 || j_factor == 0 {
+        return Vec::new();
+    } else if i_factor == 1 && j_factor == 1 {
+        return Vec::from(src);
+    }
+
+    let height = src.len() / width;
+
+    let new_width = width.div_ceil(j_factor);
+    let new_height = height.div_ceil(i_factor);
+
+    let mut dst: Vec<u8> = vec![0; new_width * new_height];
+    let block_len = i_factor * j_factor;
+
+    for y in 0..new_height {
+        for x in 0..new_width {
+            let base_src_index = y * i_factor * width + x * j_factor;
+
+            let mut block_sum: usize = 0;
+            for i in 0..i_factor {
+                for j in 0..j_factor {
+                    block_sum += src[base_src_index + i * width + j] as usize;
+                }
+            }
+
+            dst[y * new_width + x] = (block_sum / block_len) as u8;
+        }
+    }
+
+    return dst;
 }
